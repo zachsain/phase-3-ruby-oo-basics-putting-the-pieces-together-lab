41cc49d2b8b960ece13284ee53d08b8062528342
Zachary Sain <zachsain@gmail.com>
2022-03-30 14:03:15 -0500

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,32 @@
+require 'pry'
+class Book 
 
+    attr_accessor :title
+    attr_accessor :author
+    attr_reader :page_count
+    attr_reader :genre
 
+    def initialize(title)
+        @title = title
+    end 
+
+    def author=(value)
+        @author = value
+    end 
+
+    def page_count=(value)
+        @page_count = value
+    end 
+    
+    def genre=(value)
+        @genre = value
+    end 
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+end 
+
+c1 = Book.new("down the road")
+
+c1.author = "jane doe"
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,37 @@
 # Make your shoe class here!
+
+class Shoe 
+
+    attr_reader :brand
+    attr_reader :color
+    attr_reader :size
+    attr_reader :material
+    attr_reader :condition
+
+    def initialize(brand)
+        @brand = brand
+    end 
+    def color=(color)
+        @color = color
+    end 
+    def size=(size)
+        @size = size
+    end 
+    def material=(material)
+        @material = material
+    end 
+    def condition=(condition)
+        @condition = condition
+    end 
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end 
+
+
+end 
+
+c2 = Shoe.new("nike")
+c2.material = "suede"
+c2.condition = "new"
